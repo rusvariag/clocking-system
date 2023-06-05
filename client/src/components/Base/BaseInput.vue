88d25e10cf42bcce9512fd78b4ee1858f22887e8
Ivan Savin <you@example.com>
2023-06-05 09:17:43 +0300

    feat(client): add base components

---
+++ client/src/components/Base/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="wrapperClass">
+    <label v-if="label" :for="name" :class="labelClass">{{ label }}</label>
+    <input
+      :type="type"
+      :value="modelValue"
+      :name="name"
+      :placeholder="placeholder"
+      :class="inputClass"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <div v-if="error" :class="errorClass">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    modelValue: [String, Number],
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: String,
+    label: String,
+    placeholder: String,
+    error: String,
+    wrapperClass: String,
+    labelClass: String,
+    errorClass: String,
+    inputClass: String,
+  },
+}
+</script>
